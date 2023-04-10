c2c78c36554856fc6407bc7e7cd2f6808abf00f3
icehuntmen <icehuntmen@gmail.com>
2023-04-10 15:48:26 +0300

    Change version for swagger

--- packages/market/src/swagger.ts
+++ packages/market/src/swagger.ts
@@ -2,6 +2,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as utils from './utils/utils';
+import { version } from '../../../package.json';
+
 export default async function swaggerInit(
   app: INestApplication,
   config: ConfigService
@@ -11,7 +13,7 @@ export default async function swaggerInit(
   const documentBuild = new DocumentBuilder()
     .setTitle(config.get('market.name'))
     .setDescription(description)
-    .setVersion(`v${config.get('releaseVersion')}`)
+    .setVersion(`v${version}`)
     .addBearerAuth(
       { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
       'accessToken'
