a64b15f14c75cbd90bb734b8a81e485a221e015c
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2023-05-04 12:53:31 +0300

    fix: change market fee

--- packages/common/modules/database/migrations/1677512245943-DeployContractV0.ts
+++ packages/common/modules/database/migrations/1677512245943-DeployContractV0.ts
@@ -2,5 +2,5 @@ import { DeployContractBase } from './base/deploy';
 
 export class DeployContractV0_1677512245943 extends DeployContractBase {
   public readonly version: number = 0;
-  public readonly feeValue: number = 20;
+  public readonly feeValue: number = 10;
 }
