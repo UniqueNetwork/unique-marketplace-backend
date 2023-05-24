cae2f9c075cc68e68fa516a4d6b1c4a8ad093826
Alexander Shumilin <icehuntmen@gmail.com>
2023-05-24 10:53:15 +0300

    feat: migrate old market_trade to offer_events table
    - migrate to offers table
    - migrate to offer_events table

---
+++ packages/common/modules/database/migrations/1684739540519-MarketTradeToOfferEvents.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { OfferEntity, OfferEventEntity } from '../entities';
+
+export class MarketTradeToOfferEvents1684739540519 implements MigrationInterface {
+  name: 'MarketTradeToOfferEvents1684739540519';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const tableExists = await queryRunner.hasTable('market_trade');
+    if (!tableExists) {
+      console.log('Table market_trade does not exist.');
+      return;
+    }
+    await queryRunner.startTransaction();
+    try {
+      const eventsTable = queryRunner.manager.getRepository(OfferEventEntity).metadata.tableName;
+      await queryRunner.query(`
+        INSERT INTO ${eventsTable} (offer_id, event_type, block_number, address, amount, commission, collection_mode, network, created_at,updated_at )
+        SELECT ID AS offer_id, 'Buy' AS event_type, block_number_buy AS block_number, address_buyer AS address, 1 AS amount, 10 AS commission, 'NFT' AS collection_mode,
+        network, buy_created_at AS created_at,ask_created_at AS updated_at FROM market_trade
+    `);
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      throw err;
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const eventsTable = queryRunner.manager.getRepository(OfferEventEntity).metadata.tableName;
+    await queryRunner.query(`DELETE FROM ${eventsTable} WHERE network = 'quartz';`);
+  }
+}
