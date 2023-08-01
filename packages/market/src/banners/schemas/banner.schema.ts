e506c4f44fecd29492e2fb2638e5e82b23d87914
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2023-08-01 13:57:52 +0300

    fix: api banners

--- packages/market/src/banners/schemas/banner.schema.ts
+++ packages/market/src/banners/schemas/banner.schema.ts
@@ -1,5 +1,5 @@
 import { SchemaObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
-import { ButtonTitleMaxLen, DescriptionMaxLen, TitleMaxLen } from '../types';
+import { ButtonDefaultColor, ButtonTitleMaxLen, DescriptionMaxLen, FileMaxSize, TitleMaxLen } from '../types';
 import { BannerEntity } from '@app/common/modules/database';
 
 type BannerKeys = Exclude<keyof BannerEntity, 'id' | 'minioFile' | 'createdAt'>;
@@ -8,58 +8,54 @@ const properties: Record<BannerKeys | 'file', SchemaObject> = {
   file: {
     type: 'string',
     format: 'binary',
-    description: `Image, GIF or video for the banner; requiredMax size 100 MB; supported formats: ___Recommended: 800:600 pixels`,
+    description: `The banner picture; required; max size ${FileMaxSize} MB; supported formats: png, jpg, gif, svg, .mp4, webm, ogv, HTML; aspect ratio: 3*4`,
   },
   title: {
-    description: `Title for the banner; requiredMax ${TitleMaxLen} symbols`,
+    description: `Title for the banner; required; max ${TitleMaxLen} symbols`,
     type: 'string',
-    maximum: 3,
   },
   description: {
-    description: `Text for the banner; requiredMax ${DescriptionMaxLen} symbols`,
+    description: `Text for the banner; required; max ${DescriptionMaxLen} symbols; you can use html tags`,
+    type: 'string',
+  },
+  backgroundColor: {
+    description: 'The color of the banner background; HEX; optional; remember: the text is always black, use light colors',
     type: 'string',
   },
   buttonTitle: {
-    description: `Text on the button; required.Recommended: 'Explore the collection'; Max ${ButtonTitleMaxLen} symbols`,
+    description: `Text on the button; required; recommended: 'Explore the collection'; Max ${ButtonTitleMaxLen} symbols`,
     type: 'string',
   },
   buttonUrl: {
-    description: 'Link for the button; required;  Recommended: the marketplace collection page link',
+    description: 'Link for the button; required; recommended: the marketplace collection page link',
     type: 'string',
   },
   buttonColor: {
-    description: 'Color for the button; optional',
+    description: `Color for the button; optional; default ${ButtonDefaultColor}; HEX; remember: the text is always white`,
+    type: 'string',
+  },
+  secondaryButtonTitle: {
+    description: `Text on the button, optional; max ${ButtonTitleMaxLen} symbols`,
+    type: 'string',
+  },
+  secondaryButtonUrl: {
+    description: 'Link for the button; optional',
     type: 'string',
   },
   sortIndex: {
-    description: 'Index for sorting; optional, default 0. Recommended: 100; 150: 200; 150.',
+    description:
+      'Index for sorting; optional:;default 0; Recommended: 100 (for the first new banner #1 in the carousel) 200 (for the first new banner #2 in the carousel), 150 (to put new banner between them), 50 (to put new banner first) etc.',
     type: 'number',
   },
   off: {
     description:
-      'Temporarily turn off this banner; optional, default false. Attention! When set to "true", the sortIndex will be cleared, when re-enabled, be sure to set sortIndex again',
+      'Temporarily turn off this banner; optional, default false. Attention! When set to "true", the sortIndex will be cleared, when re-enabled, be sure to set new sortIndex',
     type: 'boolean',
   },
   collectionId: {
     description:
       'Collection ID, optional. If set, the collection will appear in the "Curated collections" filter on the main page',
     type: 'number',
-  },
-  secondaryButtonTitle: {
-    description: `Text on the secondary button; optional.Recommended: 'Explore the collection'; Max ${ButtonTitleMaxLen} symbols`,
-    type: 'string',
-  },
-  secondaryButtonUrl: {
-    description: 'Link for the secondary button; optional;  Recommended: the marketplace collection page link',
-    type: 'string',
-  },
-  secondaryButtonColor: {
-    description: 'Color for the secondary button; optional',
-    type: 'string',
-  },
-  backgroundColor: {
-    description: 'Background color; optional',
-    type: 'string',
   },
 };
 
