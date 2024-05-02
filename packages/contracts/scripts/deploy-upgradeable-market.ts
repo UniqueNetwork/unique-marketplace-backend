403189f723a37f66524b8813fee9fd378143665a
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2024-05-02 13:17:08 +0300

    feat: add gas limit

--- packages/contracts/scripts/deploy-upgradeable-market.ts
+++ packages/contracts/scripts/deploy-upgradeable-market.ts
@@ -4,9 +4,16 @@ import { ethers, upgrades } from 'hardhat';
 
 async function main() {
   const Market = await ethers.getContractFactory('Market');
+
   console.log('Deploying Market...');
-  const box = await upgrades.deployProxy(Market, [0], { initializer: 'initialize' });
-  await box.deployed();
+  const box = await upgrades.deployProxy(Market, [0], {
+    initializer: 'initialize',
+    txOverrides: {
+      gasLimit: 7000000,
+    },
+  });
+
+  await box.waitForDeployment();
   console.log('Market deployed to:', box.address);
 }
 
