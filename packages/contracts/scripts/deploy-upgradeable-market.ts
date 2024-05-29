8570f0fa2e53e0ca18d3da7eb8a2edb333371d31
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2024-05-29 03:06:40 +0300

    fix: add admin to upgradeable contract

--- packages/contracts/scripts/deploy-upgradeable-market.ts
+++ packages/contracts/scripts/deploy-upgradeable-market.ts
@@ -1,19 +1,42 @@
-
 import { ethers, upgrades } from 'hardhat';
+import { KeyringProvider } from '@unique-nft/accounts/keyring';
+import { Market } from '../typechain-types';
+import { Address } from '@unique-nft/utils';
+
+const seed = process.env.SUBSTRATE_SIGNER_SEED!;
 
 async function main() {
-  const Market = await ethers.getContractFactory('Market');
+  const MarketFactory = await ethers.getContractFactory('Market');
 
   console.log('Deploying Market...');
-  const box = await upgrades.deployProxy(Market, [0], {
+  const contract = await upgrades.deployProxy(MarketFactory, [0], {
     initializer: 'initialize',
     txOverrides: {
       gasLimit: 7000000,
     },
   });
 
-  await box.waitForDeployment();
-  console.log('Market deployed to:', await box.getAddress());
+  await contract.waitForDeployment();
+  console.log('Market deployed to:', await contract.getAddress());
+
+  await addAdmin(contract as unknown as Market);
+}
+
+async function addAdmin(market: Market) {
+  console.log('adding admin to contract');
+  const provider = new KeyringProvider({
+    type: 'sr25519',
+  });
+  const signer = provider.addSeed(seed);
+  const address = signer.address;
+  const ethAddress = Address.mirror.substrateToEthereum(address);
+
+  const tx = await market.addAdmin(ethAddress, {
+    gasLimit: 7_000_000,
+  });
+  await tx.wait();
+
+  console.log(`added admin sub: ${address}, eth: ${ethAddress}`);
 }
 
 main();
