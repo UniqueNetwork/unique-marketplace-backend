cb2825c5232a823925f2245b85822cdb31d1e1f2
ashkuc <ashkucc@gmail.com>
2025-10-08 14:40:43 +0200

    do not change old migrations

--- packages/common/modules/database/migrations/1679578453871-OffersTable.ts
+++ packages/common/modules/database/migrations/1679578453871-OffersTable.ts
@@ -19,7 +19,6 @@ export class OffersTable1679578453871 implements MigrationInterface {
           { name: 'price_parsed', type: 'numeric', precision: 38, scale: 18 },
           { name: 'price_raw', type: 'varchar' },
           { name: 'amount', type: 'integer' },
-          { name: 'currency', type: 'integer', default: 0 },
           { name: 'contract_address', type: 'varchar' },
           {
             name: 'status',
--- packages/common/modules/database/migrations/1716368384726-AddCurrencyToOffer.ts
+++ packages/common/modules/database/migrations/1716368384726-AddCurrencyToOffer.ts
@@ -6,14 +6,8 @@ export class AddCurrencyToOffer1716368384726 implements MigrationInterface {
   name: 'AddCurrencyToOffer1716368384726';
   public async up(queryRunner: QueryRunner): Promise<any> {
     // language=SQL format=false
-    const isColumnExist = await queryRunner.hasColumn(queryRunner.manager.getRepository(OfferEntity).metadata.tableName, 'currency');
-
-    if (isColumnExist) {
-      return;
-    }
-
-      await queryRunner.query(`ALTER TABLE ${queryRunner.manager.getRepository(OfferEntity).metadata.tableName}
-        ADD currency integer default 0;`);
+    await queryRunner.query(`ALTER TABLE ${queryRunner.manager.getRepository(OfferEntity).metadata.tableName}
+ADD currency integer default 0;`);
 
     await queryRunner.query(`
     CREATE OR REPLACE VIEW
